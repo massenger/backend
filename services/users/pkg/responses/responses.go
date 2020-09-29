5effc2b31134807d6715f683e4bdb43d508c468d
zanefinner <zanefinnergm@gmail.com>
2020-09-29 17:47:15 -0400

    create and read users

--- services/users/pkg/responses/responses.go
+++ services/users/pkg/responses/responses.go
@@ -5,16 +5,17 @@ import "gorm.io/gorm"
 //Response is formatting for the file service
 type Response struct {
 	gorm.Model
-	UserName     `json:"username"`
-	PasswordHash `json:"passwordhash"`
+	Ok           bool   `json:"ok"`
+	UserName     string `json:"username"`
+	PasswordHash string `json:"passwordhash"`
 }
 
 //Create ...
 func Create() Response {
-	return Requests{}
+	return Response{}
 }
 
 //CreatePointer ...
 func CreatePointer() *Response {
-	return &Requests{}
+	return &Response{}
 }
--- services/users/pkg/service/service.go
+++ services/users/pkg/service/service.go
@@ -1,29 +1,93 @@
 package services
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/massenger/backend/services/users/pkg/requests"
+	"github.com/massenger/backend/services/users/pkg/responses"
+	"github.com/massenger/backend/services/users/pkg/storage"
 	"gorm.io/gorm"
 )
 
 //Get ...
 func Get(r *http.Request, id string, db *gorm.DB) string {
-	if true {
-		log.Println("files\\get\\fail")
-		return "fail!"
+	response := responses.Create()
+	idAsInt, err := strconv.ParseInt(id, 0, 64)
+	if err != nil {
+		response.Ok = false
+		responseByte, _ := json.Marshal(response)
+		responseString := string(responseByte)
+		log.Println("users\\get\\fail")
+		return responseString
 	}
-	return ""
+	if idAsInt == 0 {
+		//list in future
+		response.Ok = false
+		responseByte, _ := json.Marshal(response)
+		responseString := string(responseByte)
+		log.Println("users\\get\\fail")
+		return responseString
+	}
+	user := &storage.User{}
+	db.First(&user, idAsInt)
+	if user.ID == 0 {
+		//not found
+		response.Ok = false
+		responseByte, _ := json.Marshal(response)
+		responseString := string(responseByte)
+		log.Println("users\\get\\fail")
+		return responseString
+	}
+	response.ID = user.ID
+	response.UserName = user.UserName
+	response.Ok = true
+	responseByte, _ := json.Marshal(response)
+	responseString := string(responseByte)
+	log.Println("files\\get\\success")
+	return responseString
+
 }
 
 //Post ...
 func Post(r *http.Request, db *gorm.DB) string {
-	if true {
-		log.Println("files\\post\\fail")
-		return "fail!"
+	//validation to be updated...
+	defer r.Body.Close()
+	request := requests.CreatePointer()
+	body, err := ioutil.ReadAll(r.Body)
+	json.Unmarshal(body, &request)
+	err = json.Unmarshal(body, &request)
+	response := responses.Create()
+	if err != nil {
+		response.Ok = false
+		responseByte, _ := json.Marshal(response)
+		responseString := string(responseByte)
+		log.Println("users\\post\\fail")
+		return responseString
 	}
-	return ""
+	//search request.ID
+	//find or dont find
+
+	if valid(request) {
+		user := &storage.User{}
+		user.UserName = request.UserName
+		//more desc to come!
+		db.Create(user)
+		response.ID = user.ID
+		response.Ok = true
+		responseByte, _ := json.Marshal(response)
+		responseString := string(responseByte)
+		log.Println("users\\post\\success")
+		return responseString
+	}
+	response.Ok = false
+	responseByte, _ := json.Marshal(response)
+	responseString := string(responseByte)
+	log.Println("users\\post\\fail")
+	return responseString
 }
 
 //Put ...
